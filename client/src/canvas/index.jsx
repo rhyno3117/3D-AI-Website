397b37504a9e303e455b93344dbcef2dc195d25d
ryan <rhynogolfer42@gmail.com>
2024-02-23 19:34:29 -0800

    Adds myfile.md

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,17 +1,24 @@
 import { Canvas } from '@react-three/fiber'
-import { Enviornment, Center } from '@react-three/drei'  
+import { Environment, Center } from '@react-three/drei';
 
 import Shirt from './Shirt';
 import Backdrop from './Backdrop';
 import CameraRig from './CameraRig';
 
-
 const CanvasModel = () => {
   return (
     <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
 
+      {/* <CameraRig> */}
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      {/* </CameraRig> */}
     </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel

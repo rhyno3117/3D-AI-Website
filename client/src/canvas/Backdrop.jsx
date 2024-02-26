a59c061caa6ad5e06eff698035e93b330b628b9a
ryan <rhynogolfer42@gmail.com>
2024-02-25 19:35:25 -0800

    Finished Shirt

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { easing } from 'maath'
+import { useFrame } from '@react-three/fiber'
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
 
 const Backdrop = () => {
+
   return (
-    <div>Backdrop</div>
+   <div>
+    Back
+   </div>
   )
 }
 
